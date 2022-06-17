97af29ebd858e28cddd423ae9e0324ab33a1483d
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-17 22:59:37 +0100

    adding css style

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,27 +3,28 @@ const express = require("express");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs"); // checks into the views folder for ejs file
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
-    var today = new Date();
-    // var currentDay = today.getDay();
-    // var day = "";
+    let today = new Date();
+    // let currentDay = today.getDay();
+    // let day = "";
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
 
-    res.render("list", { kindOfDay: day, newListItems: items }); //kindOfDay = to variable made in the list.ejs file
+    res.render("list", { kindOfDay: day, newListItems: items }); //kindOfDay = to letiable made in the list.ejs file
 });
 
 
