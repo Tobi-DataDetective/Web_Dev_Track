a86a2c5c3cdd1c7f158735bae5474a88a4c15cf2
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-06-20 10:23:24 +0100

    MongoDB NodeJS

--- MongoDB/FruitsProject/app.js
+++ MongoDB/FruitsProject/app.js
@@ -1,20 +1,53 @@
-// jshint esversion: 6
-
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert'); //Assert as to do with testing
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'fruitsDB';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
     const db = client.db(dbName);
+    const collection = db.collection('fruits');
 
-    client.close();
-});
+    // the following code examples can be pasted here...
+
+    // // inserting values to the document
+    // const insertResult = await collection.insertMany([{
+    //         name: "Apple",
+    //         score: 8,
+    //         review: "Great Fruit"
+    //     }, {
+    //         name: "Orange",
+    //         score: 6,
+    //         review: "Kinda Sour"
+    //     }, {
+    //         name: "Banana",
+    //         score: 9,
+    //         review: "Great Stuff!"
+    //     }
+
+    // ]);
+    // console.log('Inserted documents =>', insertResult);
+
+
+    // Finding all documents
+
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
+
+    return 'done.';
+}
+
+
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
