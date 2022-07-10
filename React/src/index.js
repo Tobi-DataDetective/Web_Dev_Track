9c7983067c6a981663a2814ebdce0813222b049b
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-10 14:59:01 +0100

    react file

---
+++ React/src/index.js
@@ -0,0 +1,52 @@
+// import React from "react";
+// import ReactDom from "react-dom";
+
+// const number = 7;
+
+// const fName = "Tobi";
+// const lName = "Folayan";
+
+
+// // ReactDom.render(WHAT TO SHOW, WHERE TO SHOW IT)
+// ReactDom.render(
+//   <div>
+//     <h1 className="heading">Hello {fName +" "+ lName}</h1>
+//     <p>This is your lucky number {number}</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+
+// ///////////////////////////////////////////////////////////////////////////////////
+// import React from "react";
+// import ReactDom from "react-dom";
+
+
+// const fName = "Tobi";
+// const currentDate = new Date();
+// const year = currentDate.getFullYear();
+
+// ReactDom.render(
+//     <div>
+//         <h1>created By {fName}</h1>
+//         <p>Copyright {year}</p>
+//     </div>,
+//     document.getElementById("root")
+// );
+/////////////////////////////////////////////////////////////////
+
+import React from "react";
+import ReactDom from "react-Dom";
+
+ReactDom.render( <
+    div >
+    <
+    h1 > My Favorite food < /h1> <
+    ul >
+    <
+    li > Beacon < /li> <
+    li > Jamon < /li>     <
+    li > Noodles < /li>     <
+    /ul>     <
+    /div>
+)
