05d61425f8a2a7c210b8a3ee7859f645b23f88af
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-16 09:09:13 +0100

    react styling

--- ReactApp/my-app/src/index.js
+++ ReactApp/my-app/src/index.js
@@ -1,23 +1,42 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// ReactDOM.render(what to show, where to show it);
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+ReactDOM.render( <
+        div >
+        <
+        h1 className = "heading"
+        contentEditable = "true"
+        spellCheck = "true" > The Four Elements < /h1> <
+        div >
+        <
+        img className = "images"
+        src = "https://tse2.mm.bing.net/th?id=OIP.fI2fmPYOCpauNGiinhPBxwAAAA&pid=Api"
+        alt = "Earth" / >
+        <
+        img className = "images"
+        src = "https://tse1.mm.bing.net/th?id=OIP.wO9v1OETB190w4T2KidjFgAAAA&pid=Api"
+        alt = "Water" / >
+        <
+        img className = "images"
+        src = "https://tse2.mm.bing.net/th?id=OIP.MQhUr2rdodTtBe6GVV_bGQAAAA&pid=Api"
+        alt = "Fire" / >
+        <
+        img className = "images"
+        src = "https://tse1.mm.bing.net/th?id=OIP.TcEMHSNhXAp0DVP9LhnnGwAAAA&pid=Api"
+        alt = "Air" / >
+        <
+        /div> <
+        /div>,document.getElementById("root"));
 
+        // const name = "Tobi";
+        // const currentDate = new Date(2022, 6, 1);
+        // const year = currentDate.getFullYear();
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render( < h1 > Hello World < /h1>, document.getElementById('root'));
+        // ReactDOM.render(
+        // <div>
+        // <h1>Created By {name}</h1>
+        // <p> copyright {year}</p>
+        // </div>
+        // ,document.getElementById('root'));
