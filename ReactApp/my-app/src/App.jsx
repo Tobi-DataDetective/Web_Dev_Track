a081da92aec74782beea05d0668a0798351a8e6e
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-16 10:50:27 +0100

    react components

---
+++ ReactApp/my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Heading from "./Heading";
+import Lists from "./Lists";
+
+function App() {
+    return ( <
+        div >
+        <
+        Heading / >
+        <
+        Lists / >
+        <
+        /div>
+    );
+}
+
+export default App;
--- ReactApp/my-app/src/index.js
+++ ReactApp/my-app/src/index.js
@@ -1,30 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReacttDOM from "react-dom";
+import App from "./App";
 
-const date = new Date();
-const currentTime = date.getHours();
 
-let day;
-
-const customeStyling = {
-    color: ""
-};
-
-if (currentTime < 12) {
-    day = "Morning";
-    customeStyling.color = "blue";
-} else if (currentTime < 18) {
-    day = "Afternoon";
-    customeStyling.color = "red";
-} else {
-    day = "Evening";
-    customeStyling.color = "black";
-}
-
-ReactDOM.render( <
-    h1 className = "heading"
-    style = { customeStyling } >
-    Good { day } <
-    /h1>,
-    document.getElementById('root')
-);
+ReacttDOM.render( <
+    App / > ,
+    document.getElementById('root'));
