3846cb3f3eadd5f81a54ca2570cfce474723623d
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-16 10:08:35 +0100

    day time conditions

--- ReactApp/my-app/src/index.js
+++ ReactApp/my-app/src/index.js
@@ -1,42 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// ReactDOM.render(what to show, where to show it);
+const date = new Date();
+const currentTime = date.getHours();
+
+let day;
+
+const customeStyling = {
+    color: ""
+};
+
+if (currentTime < 12) {
+    day = "Morning";
+    customeStyling.color = "blue";
+} else if (currentTime < 18) {
+    day = "Afternoon";
+    customeStyling.color = "red";
+} else {
+    day = "Evening";
+    customeStyling.color = "black";
+}
 
 ReactDOM.render( <
-        div >
-        <
-        h1 className = "heading"
-        contentEditable = "true"
-        spellCheck = "true" > The Four Elements < /h1> <
-        div >
-        <
-        img className = "images"
-        src = "https://tse2.mm.bing.net/th?id=OIP.fI2fmPYOCpauNGiinhPBxwAAAA&pid=Api"
-        alt = "Earth" / >
-        <
-        img className = "images"
-        src = "https://tse1.mm.bing.net/th?id=OIP.wO9v1OETB190w4T2KidjFgAAAA&pid=Api"
-        alt = "Water" / >
-        <
-        img className = "images"
-        src = "https://tse2.mm.bing.net/th?id=OIP.MQhUr2rdodTtBe6GVV_bGQAAAA&pid=Api"
-        alt = "Fire" / >
-        <
-        img className = "images"
-        src = "https://tse1.mm.bing.net/th?id=OIP.TcEMHSNhXAp0DVP9LhnnGwAAAA&pid=Api"
-        alt = "Air" / >
-        <
-        /div> <
-        /div>,document.getElementById("root"));
-
-        // const name = "Tobi";
-        // const currentDate = new Date(2022, 6, 1);
-        // const year = currentDate.getFullYear();
-
-        // ReactDOM.render(
-        // <div>
-        // <h1>Created By {name}</h1>
-        // <p> copyright {year}</p>
-        // </div>
-        // ,document.getElementById('root'));
+    h1 className = "heading"
+    style = { customeStyling } >
+    Good { day } <
+    /h1>,
+    document.getElementById('root')
+);
