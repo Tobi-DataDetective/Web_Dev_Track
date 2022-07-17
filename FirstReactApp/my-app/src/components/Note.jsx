4354e22ee04b2857c71e7fbc6d14cecfcc6ebb7b
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-17 10:01:07 +0100

    Keeper App solution1

---
+++ FirstReactApp/my-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>The Last Air Bender</h1>
+      <p>Earth, Water, Air and fire the whiler of the four elements</p>
+    </div>
+  );
+}
+
+export default Note;
