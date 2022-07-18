51142ef2c4a5403e0e4bcd175e6c152789bb62c4
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-18 10:37:02 +0100

    refactor emoji app

--- Emoji_App/my-app/src/components/App.jsx
+++ Emoji_App/my-app/src/components/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Card from "./Card";
 import emojipedia from "../emojipedia";
 
-function createCard(emojipedia) {
-  return (
-    <Card
-      key={emojipedia.id}
-      emoji={emojipedia.emoji}
-      name={emojipedia.name}
-      meaning={emojipedia.meaning}
-    />
-  );
-}
+// function createCard(emojipedia) {
+//   return (
+//     <Card
+//       key={emojipedia.id}
+//       emoji={emojipedia.emoji}
+//       name={emojipedia.name}
+//       meaning={emojipedia.meaning}
+//     />
+//   );
+// }
 
 function App() {
   return (
@@ -20,7 +20,16 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojipedia) => (
+          <Card
+            key={emojipedia.id}
+            emoji={emojipedia.emoji}
+            name={emojipedia.name}
+            meaning={emojipedia.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
