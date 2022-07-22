75fc7637a29d39512482cd82e7e9dd06e6195056
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-22 11:11:24 +0100

    managing component, spread, material ui

---
+++ es6-spread-operator-forked/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// understanding Spread
+
+// const citrus = ["Lemon", "Lime", "Orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
+// console.log(fruits);
+
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   userName: "Jamesbond007"
+// };
+
+// console.log(user);
