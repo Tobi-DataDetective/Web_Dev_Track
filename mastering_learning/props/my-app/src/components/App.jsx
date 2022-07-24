c34ff668e8fbe9ae7784dbfd04cc6f854b1f4b22
Tobi-DataDetective <folayantobitaiwo@gmail.com>
2022-07-24 21:57:16 +0100

    delete individual button handled

--- mastering_learning/props/my-app/src/components/App.jsx
+++ mastering_learning/props/my-app/src/components/App.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Card from "./Card";
 import Notes from "./Notes";
 
-function notesToCard(Notes) {
-  return <Card title={Notes.title} content={Notes.content} />;
-}
+// a mapping helper function, with just 2 intakes i.e title and
 
 function App() {
+  // Initial state of the Notes available
+  const [availableNote, setNotes] = useState(Notes);
+
+  //   helper function to delete a note for the availableNotes
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((notes, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {Notes.map(notesToCard)}
+      {availableNote.map((notes, index) => {
+        return (
+          <Card
+            key={index}
+            id={index}
+            title={notes.title}
+            content={notes.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
